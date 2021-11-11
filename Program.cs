d0395c87b7c89c9c94d6a4e8c6e018947194db0a
SARATHCHANDRA <sarathchandra76@gmail.com>
2021-11-11 17:13:41 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// abstract - class demo
+namespace Demo
+{
+    // abstract class will share common code to it's sub-classes.
+    public abstract class Shape
+    {
+        public string Color { get; set; }
+        public int Thickness { get; set; }
+
+        public abstract void Draw();
+    }
+
+    public class Line:Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draws Line");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draws Rectangle");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Shape shape1 = new Line { Color = "red", Thickness = 1 };
+            Shape shape2 = new Rectangle { Color = "red", Thickness = 2 };
+
+            shape1.Draw();
+            shape2.Draw();
+        }
+    } 
+}    
